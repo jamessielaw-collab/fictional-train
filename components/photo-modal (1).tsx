bdc305a5ba9aa16e80bad1571391db087085d44c
jamessielaw-collab <jamessielaw@yahoo.com>
2025-10-30 21:40:48 +0100

    Update photo-modal (1).tsx

--- components/photo-modal (1).tsx
+++ components/photo-modal (1).tsx
@@ -10,7 +10,7 @@ const images = [
   "/gallery-3.jpg",
   "/gallery-4.jpg",
   "/gallery-5.jpg",
-  "/gallery-6.jpg",
+  "/gallery-6-min.jpg",
   "/gallery-7.jpg",
   "/gallery-8.jpg",
   "/pre-wedding.jpg",
